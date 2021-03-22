8db056a0ba0daaede1dafb7182713a1d4035d54e
massun1999 <73097630+massun1999@users.noreply.github.com>
2021-03-22 19:02:27 +0900

    レスポンシブ

--- app/javascript/slider.js
+++ app/javascript/slider.js
@@ -1,9 +1,20 @@
  window.addEventListener('load', function(){
-   console.log('load')
   $('.mv').slick({
     autoplay:true,
     autoplaySpeed:5000,
     dots:false,
+  });
+
+  $('.sp-mv').slick({
+    autoplay:true,
+    autoplaySpeed:5000,
+    dots:false,
+  });
+//ハンバーガーメニュー
+  $('.burger-btn').on('click',function(){
+    $('.burger-btn').toggleClass('close');
+    $('.nav-wrapper').fadeToggle(500);
+    $('body').toggleClass('noscroll');
   });
  });
 
