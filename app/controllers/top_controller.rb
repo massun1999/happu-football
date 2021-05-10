4794925628c8c4046c8beb4f1ee476a1ce5f57ee
massun1999 <73097630+massun1999@users.noreply.github.com>
2021-05-10 12:06:08 +0900

    投稿表示の修正

--- app/controllers/top_controller.rb
+++ app/controllers/top_controller.rb
@@ -1,7 +1,7 @@
 class TopController < ApplicationController
   def index
-    @threds = Thred.order(created_at: :desc).last(2)
-    @columns = Column.order(created_at: :desc).last(3)
+    @threds = Thred.order(created_at: :desc).limit(2)
+    @columns = Column.order(created_at: :desc).limit(3)
     @schedule = Schedule.where(['year LIKE ?', "%#{20}%"]).last
     @month = Schedule.where.not(month: "-").last
     @schedules = Schedule.order(created_at: :desc).limit(31)
